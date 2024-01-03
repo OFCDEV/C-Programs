e6894c9e51e92b08ae561068ef371d46e9b98deb
Debasish Bhola <devop69@gmail.com>
2024-01-03 09:54:15 +0530

    Execl calls

---
+++ excelPr1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main(){
+	pid_t cpid;
+	int status =0;
+	int num=5;
+	cpid= fork();
+	if(cpid == -1)
+		exit(0);
+	if(cpid == 0){
+		printf("\nBefore exex\n");
+		execl("./program2","arg1","arg2",NULL);
+		printf("\n line is not printed\n");
+	}
+	else{
+		printf("\n Parent executing before wait(),child process created by parent is =(%d)\n",cpid);
+		cpid=wait(&status); /* waiting for child process to exit */
+		printf("\n wait() in parent done\nParent pid=%d\n",getpid());
+		printf("\n cpid retuned is (%d)\n",cpid);
+		printf("\n status is (%d)\n",status);
+	}
+	return 0;
+}
