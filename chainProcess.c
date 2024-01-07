29437227caa5ae99f16a53845f6909d5085de92b
Debasish Bhola <devop69@gmail.com>
2024-01-07 20:50:13 +0530

    bug fix

--- chainProcess.c
+++ chainProcess.c
@@ -7,8 +7,7 @@ pid_t childpid = 0;
 int i, n;
 if (argc != 2){/* check for valid number of
 command-line arguments */
-fprintf(stderr, "Usage: %s processes\n", argv
-[0]);
+fprintf(stderr, "Usage: %s processes\n", argv[0]);
 return 1;
 }
 n = atoi(argv[1]);
