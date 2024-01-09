b1dfa8dc3f7a4d4d1b1e035de8f2df98cee95a72
Debasish Bhola <devop69@gmail.com>
2024-01-09 09:03:58 +0530

    Completed criticalSection2.c

---
+++ crirticalSection2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int shrd = 0;
+void process(int inc){
+    int x=shrd;
+    x+=inc;
+    sleep(1);
+    shrd=x;
+    printf("Process: shrd = %d\n",shrd);
+}
+int main(){
+    pid_t pid1,pid2;
+    pid1=fork();
+    if (pid1 == 0)
+    {
+        process(1);
+    }else{
+        pid2 = fork();
+        if (pid2==0)
+        {
+            process(-1);
+        }else{
+            waitpid(pid1,NULL,0);
+            waitpid(pid2,NULL,0);
+            printf("Final shrd value: %d\n",shrd);
+        }
+        
+    }
+    return 0;
+}
